9a2434bc0d72d267fa41540de28c191164f817fd
Sara <up202406368@up.pt>
2025-02-22 10:38:42 +0000

    Resolução final do exercício 2.

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,17 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-            Pessoa pessoa = new Pessoa();
-            pessoa.setNome("Sara");
-            pessoa.setIdade(18);
-            pessoa.setAltura(1.64);
-            pessoa.setPeso(65.0);
-            System.out.println("Nome: " + pessoa.getNome());
-            System.out.println("Idade: " + pessoa.getIdade() + " anos");
-            System.out.println("Altura: " + pessoa.getAltura() + " metros");
-            System.out.println("Peso: " + pessoa.getPeso() + " kg");
-            System.out.println("IMC: " + pessoa.calcularIMC() + " kg/m2");
-            System.out.println("Classificação: " + pessoa.classificarIMC());
-        }
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Sara");
+        pessoa.setIdade(18);
+        pessoa.setAltura(1.64);
+        pessoa.setPeso(65.0);
+        System.out.println("Nome: " + pessoa.getNome());
+        System.out.println("Idade: " + pessoa.getIdade() + " anos");
+        System.out.println("Altura: " + pessoa.getAltura() + " metros");
+        System.out.println("Peso: " + pessoa.getPeso() + " kg");
+        System.out.println("IMC: " + pessoa.calcularIMC() + " kg/m2");
+        System.out.println("Classificação: " + pessoa.classificarIMC());
+    }
 }
+
